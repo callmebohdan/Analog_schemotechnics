1723e34ae4ce1551355db4c6cd6565f782408540
boganponomarenko <pb6798@gmail.com>
2019-09-09 13:37:21 +0300

    lab1_threaded_applications

--- dk61_ponomarenko/lab1_threaded_applications/src/lab1.c
+++ dk61_ponomarenko/lab1_threaded_applications/src/lab1.c
@@ -7,50 +7,52 @@
 
 int thread_cnt = 0, loop_length;
 
-void *thread(void *argument) {
-        
-       loop_length = *(int*)argument;
-         
-       while (thread_cnt++ < loop_length)
-        {
-		  
+void *thread(void *argument)
+{
+	loop_length = *(int*)argument;
+	while (thread_cnt++ < loop_length) {
         }
         return NULL;
 }
 
-int main(int argc, char **argv) {
-        
+int main(int argc, char **argv)
+{	
+	/* Checking if user entered data in console correctly */
         if (argc != 3) {
-		printf("Error. Invalid number of arguments %d. You need 3 to proceed", argc-1);
+		printf("Error. Invalid number of arguments. Enter again");
 		return -1;
         }
         
-        loop_length = atoi(argv[1]);
+        /* Convert entered string to integer */
+        loop_length = atoi(argv[1]);	
         
+        /* Initialize descryptor of thread */
         pthread_t threads[THREADS_MAX_AMOUNT];
-		
-	clock_t begin_of_count = clock();
+	
+	clock_t begin_count = clock();
 	
 	printf("Created two threads\n");
         
-        for (int i = 0; i < THREADS_MAX_AMOUNT; i++)
-        {
+        /* creating constant quantity of threads, defined inb4*/
+        for (int i = 0; i < THREADS_MAX_AMOUNT; i++) {
                 pthread_create(&threads[i], NULL, thread, &loop_length);
         }
+        
         printf("Waiting for threads to finish execution...\n");
-		
-        for (int j = 0; j < THREADS_MAX_AMOUNT; j++)
-        {
-                pthread_join(threads[j], NULL);
+	
+	/* waiting for the end of threads */
+        for (int j = 0; j < THREADS_MAX_AMOUNT; j++) {
+		pthread_join(threads[j], NULL);	
         }
 	
-	clock_t end_of_count = clock();
+	clock_t end_count = clock();
 	
-	double exec_time = (double)(end_of_count - begin_of_count) / CLOCKS_PER_SEC;
+	/* count time, spent for executing thread from the start to the end*/
+	double exec_time = (double)(end_count - begin_count) / CLOCKS_PER_SEC;
 	
         printf("Threads have finished their execution\n");
-
+	
         printf("Thread final incremented value is: %d\n", thread_cnt);
         
-        printf("Execution time is: %f ms\n", exec_time*1000);
+        printf("Execution time is: %f ms\n", exec_time * 1000);
 }        
